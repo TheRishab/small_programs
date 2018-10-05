72bd66a5f72fb23946c3f2c802c32e59db90bcda
TheRishab <grishab3379@gmail.com>
2018-10-05 17:49:52 +0530

    added

---
+++ c and c++/rh.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int mul()
+{
+	return a*b;
+	
+}
+
+int main() {
+	int a,b,m;
+   cout << "Hello World";
+   cin>>a;
+   cin>>b;
+   m=mul(a,b);
+   
+   cout<<m;
+   
+   
+   return 0;
+}
